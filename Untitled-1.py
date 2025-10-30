7ed5dc175d1d210bdc5c5b355aa0aa2299d8e8d5
Ethan West <sqqz0154@leeds.ac.uk>
2025-10-30 11:42:11 +0000

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -12,4 +12,34 @@ def average_stats_calculate(pokemon):
                 result = sum(empty_list)
         return result
 pokemon = input('test: ').title()
+
+print(average_stats_calculate(pokemon))
+
+def average_stats_calculate(pokemon):
+    with open ("pokemon.csv","r") as infile:
+        lines = infile.readlines()[1:]
+        empty_list = []
+        count = 0
+        result = 0
+        for line in lines:
+            split = line.strip().split(",")
+            type_1 = split[2]
+            type_2 = split[3]
+            split[4] = int(split[4])
+            if type_1 == 'Fire':
+                count = count + 1
+                empty_list.append(split[4])
+                result = sum(empty_list)
+                #print(count)
+            elif type_2 == 'Fire':
+                count = count + 1
+                empty_list.append(split[4])
+                result = sum(empty_list)
+                #print(count)
+                #print(empty_list)
+            if count > 0:
+                avtot = result / count
+                print(avtot)
+        return result
+pokemon = input('test: ').title()
 print(average_stats_calculate(pokemon))
